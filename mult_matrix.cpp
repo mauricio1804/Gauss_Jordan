55e77629aeef08bc2bc11178e0b897571aac0591
mauricio1804 <maumauaz18@gmail.com>
2023-11-30 22:54:43 -0300

    .

--- mult_matrix.cpp
+++ mult_matrix.cpp
@@ -23,12 +23,25 @@ void soma_matriz(int v[][C], int v2[][C]){
 
 void mult_matriz(int v[][C], int v2[][C]){
 
-    int i, j, multiplicacao[L][C];
+    int i, j, k, aux, multiplicacao[L][C];
     for (i = 0; i < C; i++){
         for (j = 0; j < L; j++){
-            
+            multiplicacao[i][j] = 0;
+            for (k = 0; k < L; k++){
+                aux += v[i][k] * v2[k][j];
+            }
+            multiplicacao[i][j] = aux;
+            aux = 0;
         }
     }
+
+    for (i = 0; i < C; i++){
+        for (j = 0; j < L; j++){
+           cout << multiplicacao[i][j] << " ";
+        } 
+        cout << endl;
+    }
+    
 }
 
 void preenche_matriz(int v[][C]){
@@ -49,8 +62,8 @@ int main(){
     preenche_matriz(v);
     preenche_matriz(v2);
 
-    cout << 3;
-
     soma_matriz(v, v2);
+
+    mult_matriz(v, v2);
 
 }
